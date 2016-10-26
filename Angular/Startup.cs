a230ce804f1dbedfe58e939b283355712acb5b63
Tran, Nhan <nhan@cdbaby.com>
2016-10-26 16:00:44 -0700

    Init

---
+++ Angular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Angular.Startup))]
+namespace Angular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
